2dd1c9404eb9daa4a037886e8f1c0338bacd5782
Hunter Jacobson <hunterdjacobson@gmail.com>
2026-06-21 02:04:13 -0400

    build: vercel web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 
+import { Analytics } from '@vercel/analytics/react';
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
